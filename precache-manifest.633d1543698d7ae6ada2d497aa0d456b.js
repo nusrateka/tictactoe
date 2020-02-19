e139eae38085fe27d028cb42c4f5c63adb1506ce
Nusrat Jahan Eka <nusrat.jahaneka@unipro.co.jp>
2020-02-19 21:18:21 +0900

    Updates

---
+++ precache-manifest.633d1543698d7ae6ada2d497aa0d456b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22bc4712b202157866a67f226aeb4104",
+    "url": "/tictactow/index.html"
+  },
+  {
+    "revision": "c549593d3bb17b0c645e",
+    "url": "/tictactow/static/css/main.30820ede.chunk.css"
+  },
+  {
+    "revision": "429e1b713229c6fc06c7",
+    "url": "/tictactow/static/js/2.d267f7db.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictactow/static/js/2.d267f7db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c549593d3bb17b0c645e",
+    "url": "/tictactow/static/js/main.89e08f9f.chunk.js"
+  },
+  {
+    "revision": "cf1f85a77b4d6c7b580f",
+    "url": "/tictactow/static/js/runtime-main.1db91cf5.js"
+  }
+]);
